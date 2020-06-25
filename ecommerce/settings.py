999ea245d4941613c405819ea7cc5683c7346b32
Tianhao Wu <hans@Tianhaos-MBP.home>
2020-06-24 22:14:48 -0400

    try without settings folder

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -145,3 +145,14 @@ MEDIA_ROOT=os.path.join(os.path.dirname(BASE_DIR), 'static_cdn', 'media_root')
 
 
 LOGOUT_REDIRECT_URL= '/accounts/templates/registration/login/'
+
+
+# CORS_REPLACE_HTTPS_REFERER      = False
+# HOST_SCHEME                     = "http://"
+# SECURE_PROXY_SSL_HEADER         = None
+# SECURE_SSL_REDIRECT             = False
+# SESSION_COOKIE_SECURE           = False
+# CSRF_COOKIE_SECURE              = False
+# SECURE_HSTS_SECONDS             = None
+# SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+# SECURE_FRAME_DENY               = False
